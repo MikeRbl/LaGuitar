23f1f5e114d056348027cdbc89e1902cc89b4e42
MikeRbl <maicoalf77@gmail.com>
2026-02-09 14:01:07 -0600

    integracion de persistencia de datos en el LocalStorage y actualizacion del README

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,28 @@ import Header from './Componentes/Header'
 import Footer from './Componentes/Footer'
 import Card from './Componentes/Card'
 import { db } from '../src/db/db'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
+  // 1. Función para obtener el estado inicial del localStorage
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
 
   const [customer, setCustomer] = useState({})
   const [total, setTotal] = useState(0)
   const [products, setProducts] = useState([])
   const [modal, setModal] = useState(false)
-  
-  console.log(total)
-
   const [data, setData] = useState(db)
+  
+  // 2. Inicializar el carrito con la función initialCart
+  const [cart, setCart] = useState(initialCart)
 
-  console.log(data)
-
-  const [cart, setCart] = useState([])
-
-  //useEffect(()=>{
-  //  setData(db)
-  //},[])
+  // 3. useEffect para persistir los datos cada vez que el carrito cambie
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   return (
     <div>
@@ -36,17 +38,16 @@ function App() {
 
         <div className="row mt-5">
           {data.map(guitar => (
-          <Card
-            key={guitar.id}
-            guitar={guitar}
-            cart={cart}
-            setCart={setCart}
-          />
-        ))}
-
+            <Card
+              key={guitar.id}
+              guitar={guitar}
+              cart={cart}
+              setCart={setCart}
+            />
+          ))}
         </div>
       </main>
-    <Footer/>
+      <Footer />
     </div>
   )
 }
